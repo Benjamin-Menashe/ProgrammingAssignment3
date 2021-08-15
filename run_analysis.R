3844046e7e3f612c69670a8cecf745ab5c3a54d0
Benjamin Menashe <81152368+Benjamin-Menashe@users.noreply.github.com>
2021-08-15 16:46:05 +0300

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -3,18 +3,18 @@
 
 ## load the data and merge into one big data frame
 # get list of features
-ftrs <- as.character(read.table("UCI HAR Dataset/features.txt")[[2]])
+ftrs <- as.character(read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt")[[2]])
 
 # load and merge the test data
-testdt <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = ftrs)
-testsub <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subjectid")
-testact <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "activity")
+testdt <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt", col.names = ftrs)
+testsub <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt", col.names = "subjectid")
+testact <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt", col.names = "activity")
 alltest <- data.frame(testsub, testact, testdt)
 
 # load and merge the train data
-trndt <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = ftrs)
-trnsub <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subjectid")
-trnact <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "activity")
+trndt <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt", col.names = ftrs)
+trnsub <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt", col.names = "subjectid")
+trnact <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt", col.names = "activity")
 alltrn <- data.frame(trnsub, trnact, trndt)
 
 # merge both
@@ -29,7 +29,7 @@ alldata <- alldata[,inds]
 # change subjectid and action to correct factors
 alldata$subjectid <- as.factor(alldata$subjectid)
 alldata$activity <- as.factor(alldata$activity)
-levels(alldata$activity) <- read.table("UCI HAR Dataset/activity_labels.txt")[[2]]
+levels(alldata$activity) <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt")[[2]]
 
 # save "alldata" dataset (optional)
 # write.table(alldata, "alldata.txt", row.names = F)
